21cbaf6b87a80a553726fbfdb4042939bcd78e17
meekoder <meekorusdi@gmail.com>
2021-05-11 13:49:23 -0700

    added context and text box

---
+++ src/Context.js
@@ -0,0 +1,27 @@
+import { React, createContext, useState, useEffect } from 'react';
+
+const Context = createContext();
+
+export const Provider = function({children}) {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(function() {
+    function getTodos() {
+      fetch('/api/todos')
+        .then((r) => r.json())
+        .then((todos) => setTodos(todos))
+        .catch((err) => console.log(err));
+    }
+    getTodos();
+  }, []);
+
+  return (
+    <Context.Provider value={{
+      todos
+    }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export default Context;
